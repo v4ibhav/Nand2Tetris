17a1a19107718db623a3f19c599ad6dddec82c39
Vaibhav Sharma <51675640+v4ibhav@users.noreply.github.com>
2020-07-24 19:22:53 +0530

    project4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,80 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@8192
+D=A
+@count
+M = D
+//////count upto 8192
+
+
+(LOOP)
+@counter
+M=0
+
+@SCREEN
+D=A
+@addr
+M = D /////addr  = 16384
+
+@KBD
+D=M
+@BLACK
+D;JNE
+@WHITE
+D;JEQ
+
+
+
+(BLACK)
+@counter
+D = M
+@count
+D = D-M
+@END
+D;JEQ
+
+@addr 
+A=M
+M = -1   //goto this address in addr and put -1
+@counter 
+M = M+1  //increase counter counts by 1
+
+///put addr to +1
+@1
+D=A
+@addr
+M = M+D
+@BLACK
+0;JMP
+
+
+(WHITE)
+@counter
+D = M
+@count
+D = D-M
+@END
+D;JEQ
+
+@addr 
+A=M
+M = 0  //goto this address in addr and put -1
+@counter 
+M = M+1  //increase counter counts by 1
+
+///put addr to +1
+@1
+D=A
+@addr
+M = M+D
+@WHITE
+0;JMP
+
+
+
+
+(END)
+@LOOP
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,51 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+@R0
+D=M
+@x
+M=D
+
+@R1
+D=M
+@n
+M=D
+
+@R2
+M = 0
+
+@i
+M=1
+@MUL
+M=0
+
+(LOOP)
+@i
+D=M
+@n
+D=D-M
+@STOP
+D;JGT
+
+@MUL
+D = M
+@x
+D = D+M
+@MUL
+M=D
+@i
+M=M+1
+@LOOP
+0;JMP
+
+(STOP)
+@MUL
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
